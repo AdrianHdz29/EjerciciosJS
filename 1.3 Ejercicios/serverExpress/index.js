5e30f57d11e4e2face36ff1d6e908779bb27d445
AdrianHdz29 <manuel.adrian.hernandez.moreno@gmail.com>
2025-03-04 21:06:29 -0500

    Add files via upload

--- 1.3 Ejercicios/serverExpress/index.js
+++ 1.3 Ejercicios/serverExpress/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
 
+// Obtener __dirname correctamente con módulos ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas para servir los archivos HTML desde 'views'
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
@@ -19,6 +24,8 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
+
+// Iniciar servidor
 app.listen(port, () => {
-    console.log(`Servidor ejecutandose en puerto ${port}`);
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
